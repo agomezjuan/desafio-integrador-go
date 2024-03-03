4db44f2292969349519a07593971fff04f6b9235
Alberto Gomez Juan <alberto.gomez.j@bairesdev.com>
2024-03-03 18:54:34 +0100

    feat: :white_check_mark: probando requerimientos

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
+	"github.com/agomezjuan/desafio-integrador-go/internal/tickets"
+	"fmt"
 )
 
 func main() {
-	total, err := tickets.GetTotalTickets("Brazil")
+	destino := "Czech Republic"
+
+    // Buscar el total de tickets hacia el destino
+    totalTicketsDestino, err := tickets.GetTotalTickets(destino)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Total de tickets hacia %s: %d\n\n", destino, totalTicketsDestino)
+
+
+	// Buscar el total de tickets por periodo
+	periodos := []string{"madrugada", "mañana", "tarde", "noche"}
+	for _, periodo := range periodos {
+		total, err := tickets.GetCountByPeriod(periodo)
+		if err != nil {
+			fmt.Printf("Error para el periodo %s: %v\n", periodo, err)
+			continue
+		}
+		fmt.Printf("Total de tickets en el periodo %s: %d\n", periodo, total)
+	}
+
+	// Calcular el porcentaje de viajeros
+	allTickets, err := tickets.ReadTickets("tickets.csv")
+	if err != nil {
+		panic(err)
+	}
+	var totalTickets int = len(allTickets)
+	
+	percentage, err := tickets.PercentageDestination(destino, totalTickets)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nPorcentaje de viajeros a %s: %.1f %%\n", destino, percentage)
 }
